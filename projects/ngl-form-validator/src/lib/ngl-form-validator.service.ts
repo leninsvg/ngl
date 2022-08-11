6ebf466432ce52d8b6a4142a5f28117a32738e37
leninsvg <lenin@aktiun.com>
2022-08-11 14:18:25 -0500

    Libraries of angular

---
+++ projects/ngl-form-validator/src/lib/ngl-form-validator.service.ts
@@ -0,0 +1,41 @@
+import { Inject, Injectable } from '@angular/core';
+import { FormValidatorSettings } from './form-validator.settings';
+import { AbstractControl, FormArray, FormControl, FormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NglFormValidatorService {
+
+  constructor(
+    @Inject('FORM_VALIDATOR_SETTINGS')
+    public formValidatorSettings: FormValidatorSettings) {
+  }
+
+  public validateAllFormFields(abstractControl: AbstractControl): void {
+    if (abstractControl instanceof FormControl) {
+      abstractControl.markAsTouched();
+      abstractControl.updateValueAndValidity();
+      return;
+    }
+    for (const field in (abstractControl as FormGroup).controls) {
+      const control = abstractControl.get(field);
+      if (!(control instanceof FormControl)) {
+        if (control instanceof FormGroup) {
+          this.validateAllFormFields(control);
+        } else if (control instanceof FormArray) {
+          for (const auxControl of control.controls) {
+            if (auxControl instanceof FormControl) {
+              control.updateValueAndValidity();
+            } else if (auxControl instanceof FormGroup) {
+              this.validateAllFormFields(auxControl);
+            }
+          }
+        }
+      } else {
+        control.markAsTouched();
+        control.updateValueAndValidity();
+      }
+    }
+  }
+}
